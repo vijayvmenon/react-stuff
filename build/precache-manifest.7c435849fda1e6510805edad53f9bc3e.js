5cd7643460926687d50e56743835e76742de3ec5
vijay venugopal menon <vijayvmenon@gmail.com>
2020-05-27 17:33:15 -0500

    adding build folder

---
+++ build/precache-manifest.7c435849fda1e6510805edad53f9bc3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f45913b7f934c636397cc7b3e9eafc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b31c001867e56a9595ff",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "182cd4f5838dfc929624",
+    "url": "/static/js/2.97cee8d5.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.97cee8d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31c001867e56a9595ff",
+    "url": "/static/js/main.57b30031.chunk.js"
+  },
+  {
+    "revision": "924d75cfbebb368bb9c6",
+    "url": "/static/js/runtime-main.a2e94c35.js"
+  }
+]);
