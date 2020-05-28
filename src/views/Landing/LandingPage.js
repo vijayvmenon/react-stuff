e8a1e794ff303508b35d56ad5f85feb95122aa9c
vijay venugopal menon <vijayvmenon@gmail.com>
2020-05-27 21:10:44 -0500

    adding build folder

--- src/views/Landing/LandingPage.js
+++ src/views/Landing/LandingPage.js
@@ -3,22 +3,26 @@ import { TextField, Button, AppBar, Toolbar } from "@material-ui/core";
 import { Route, Switch, Link } from "react-router-dom";
 import ToDoComponent from "../../components/Todo/todo";
 import ReduxAdvanced from "../../components/Redux/reduxAdvanced";
-
+import Home from "../../components/Home/Home"
 export default function LandingPage(props) {
   return (
     <Fragment>
       <AppBar position="static">
         <Toolbar>
-          <div>
+        <div style={{color:"white"}}>
+            <Link to="/">HOME</Link>
+          </div>
+          <div style={{marginLeft:24,color:"white"}}>
             <Link to="/todo">To do App</Link>
           </div>
-          <div style={{marginLeft:24}}>
+          <div style={{marginLeft:24,color:"white"}}>
             <Link to="/redux">Redux Advanced App</Link>
           </div>
         </Toolbar>
       </AppBar>
       <Switch>
-        <Route path="/todo" component={ToDoComponent} />
+      <Route  exact path="/" component={Home} />
+        <Route  path="/todo" component={ToDoComponent} />
         <Route path="/redux" component={ReduxAdvanced} />
       </Switch>
     </Fragment>
